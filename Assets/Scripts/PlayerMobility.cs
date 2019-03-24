4fdf8495f4689082bda02ad4c09bee459bfd74df
Csaba Kiss <m.kiss.csaba@gmail.com>
2019-03-24 14:11:31 +0000

    Added base project with some base art

---
+++ Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour
+{
+
+    public float speed;
+
+    void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position-mousePosition,Vector3.forward);
+
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+
+        float input = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed * input);
+
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
---
+++ Assets/Scripts/TurretScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour
+{
+
+    public Transform player;
+    public float speed;
+
+    void FixedUpdate()
+    {
+        float z = Mathf.Atan2((player.transform.position.y - transform.position.y), (player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+
+        transform.eulerAngles = new Vector3(0, 0, z);
+
+        GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed); //Use this line for soldiers
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
